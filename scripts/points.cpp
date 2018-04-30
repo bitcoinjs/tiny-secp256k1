84be5b8864a932994067ec9832bfeb7e31d5443b
Daniel Cousens <github@dcousens.com>
2018-05-01 00:26:10 +1000

    points; avoid null pass-around

--- scripts/points.cpp
+++ scripts/points.cpp
@@ -5,11 +5,9 @@
 #include "utils.hpp"
 
 template <typename A> struct IP { A a; bool e; std::string desc = ""; };
-template <typename A> struct PFS { uint8_t_32 a; A e; };
-template <typename A> struct PFSF { uint8_t_32 a; std::string except = ""; std::string desc = ""; };
-template <typename A> struct PA { A a; A b; A e; };
-template <typename A> struct PAS { A a; uint8_t_32 b; A e; };
-template <typename A> struct PASF { A a; uint8_t_32 b; std::string except = ""; std::string desc = ""; };
+template <typename A> struct PFS { uint8_t_32 a; A e; std::string except = ""; std::string desc = ""; };
+template <typename A> struct PA { A a; A b; A e; std::string desc = "";};
+template <typename A> struct PAS { A a; uint8_t_32 b; A e; std::string except = ""; std::string desc = ""; };
 struct PC { uint8_t_vec a; bool b; uint8_t_vec e; };
 
 // ref https://github.com/bitcoin-core/secp256k1/blob/6ad5cdb42a1a8257289a0423d644dcbdeab0f83c/src/tests.c#L2160
@@ -139,26 +137,26 @@ auto generate (const A G) {
 
 	// #L3719, -1 + 0 == -1
 	pas.push_back({ G_LESS_1, ZERO, G_LESS_1 });
-	pas.push_back({ G_LESS_1, ONE, NULLQ }); // == 0/infinity
+	pas.push_back({ G_LESS_1, ONE, NULLQ, "", "Adds to infinity" });
 	pas.push_back({ G_LESS_1, TWO, G_ONE });
 	pas.push_back({ G_LESS_1, THREE, G_TWO });
 	pas.push_back({ G_LESS_1, GROUP_ORDER_LESS_1, G_LESS_2 });
 	pas.push_back({ G_LESS_1, GROUP_ORDER_LESS_2, G_LESS_3 });
 	pas.push_back({ G_LESS_1, GROUP_ORDER_LESS_2, G_LESS_3 });
 	pas.push_back({ G_LESS_2, ONE, G_LESS_1 });
-	pas.push_back({ G_LESS_2, TWO, NULLQ });
+	pas.push_back({ G_LESS_2, TWO, NULLQ, "", "Adds to infinity" });
 	pas.push_back({ G_LESS_2, THREE, G_ONE });
-	pas.push_back({ G_ONE, GROUP_ORDER_LESS_1, NULLQ }); // == 0/infinity
-	pas.push_back({ G_ONE, GROUP_ORDER_LESS_2, G_LESS_1 }); // == -1
-	pas.push_back({ G_TWO, GROUP_ORDER_LESS_1, G_ONE }); // == 1
+	pas.push_back({ G_ONE, GROUP_ORDER_LESS_1, NULLQ, "", "Adds to infinity" });
+	pas.push_back({ G_ONE, GROUP_ORDER_LESS_2, G_LESS_1, "", "== G - 1" }); // == -1
+	pas.push_back({ G_TWO, GROUP_ORDER_LESS_1, G_ONE, "", "== 1" }); // == 1
 
-	std::vector<PASF<A>> pasf;
-	pasf.push_back({ fromUInt32<A>(0), ONE, "Expected Point", "Invalid Point" });
-	pasf.push_back({ G_ONE, GROUP_ORDER, "Expected Tweak", "Tweak >= G" });
-	pasf.push_back({ G_ONE, GROUP_ORDER_OVER_1, "Expected Tweak", "Tweak >= G" });
+	std::vector<PAS<A>> pasf;
+	pasf.push_back({ fromUInt32<A>(0), ONE, {}, "Expected Point", "Invalid Point" });
+	pasf.push_back({ G_ONE, GROUP_ORDER, {}, "Expected Tweak", "Tweak >= G" });
+	pasf.push_back({ G_ONE, GROUP_ORDER_OVER_1, {}, "Expected Tweak", "Tweak >= G" });
 
 	for (uint32_t i = 1; i < 5; ++i) {
-		bool ok;
+		bool ok = true;
 		const auto G_i = _pointFromUInt32<A>(i, ok); assert(ok);
 		const auto G_i_p1 = _pointFromUInt32<A>(i + 1, ok); assert(ok);
 
@@ -174,37 +172,16 @@ auto generate (const A G) {
 	pfs.push_back({ GROUP_ORDER_LESS_2, G_LESS_2 });
 	pfs.push_back({ GROUP_ORDER_LESS_3, G_LESS_3 });
 
-	std::vector<PFSF<A>> pfsf;
-	pfsf.push_back({ ZERO, "Expected Private", "Private key == 0" }); // #L3145, #L3684, fail, == 0
-	pfsf.push_back({ GROUP_ORDER, "Expected Point", "Private key >= G" }); // #L3115, #L3670, fail, == G
-	pfsf.push_back({ GROUP_ORDER_OVER_1, "Expected Point", "Private key >= G" }); // #L3162, #L3701, fail, >= G
-	pfsf.push_back({ UINT256_MAX, "Expected Point", "Private key >= G" }); // #L3131, #L3676, fail, > G
+	std::vector<PFS<A>> pfsf;
+	pfsf.push_back({ ZERO, {}, "Expected Private", "Private key == 0" }); // #L3145, #L3684, fail, == 0
+	pfsf.push_back({ GROUP_ORDER, {}, "Expected Point", "Private key >= G" }); // #L3115, #L3670, fail, == G
+	pfsf.push_back({ GROUP_ORDER_OVER_1, {}, "Expected Point", "Private key >= G" }); // #L3162, #L3701, fail, >= G
+	pfsf.push_back({ UINT256_MAX, {}, "Expected Point", "Private key >= G" }); // #L3131, #L3676, fail, > G
 
 	// ref https://github.com/bitcoin-core/secp256k1/blob/6ad5cdb42a1a8257289a0423d644dcbdeab0f83c/src/tests.c#L2160
 	test_ec_combine<A>(pa, pas, pfs);
 
-	return std::make_tuple(ip, pa, pas, pfs, pfsf, NULLQ);
-}
-
-template <typename T>
-auto jdE (const T& null) {
-	return [=](auto x) {
-		return jsonifyO({
-			jsonp("d", jsonify(x.a)),
-			jsonp("expected", x.e == null ? "null" : jsonify(x.e))
-		});
-	};
-}
-
-template <typename T>
-auto jPDE (const T& null) {
-	return [=](auto x) {
-		return jsonifyO({
-			jsonp("P", jsonify(x.a)),
-			jsonp("d", jsonify(x.b)),
-			jsonp("expected", x.e == null ? "null" : jsonify(x.e))
-		});
-	};
+	return std::make_tuple(ip, pa, pas, pasf, pfs, pfsf);
 }
 
 template <typename A, typename B>
@@ -214,41 +191,56 @@ void dumpJSON (
 	const B& uncompressed,
 	const std::vector<PC>& pc
 ) {
-	const auto jPFSF = [](auto x) {
+	const auto jIP = [](auto x) {
+		return jsonifyO({
+			x.desc.empty() ? "" : jsonp("description", jsonify(x.desc)),
+			jsonp("P", jsonify(x.a)),
+			jsonp("expected", jsonify(x.e))
+		});
+	};
+	const auto jPA = [](auto x) {
+		return jsonifyO({
+			x.desc.empty() ? "" : jsonp("description", jsonify(x.desc)),
+			jsonp("P", jsonify(x.a)),
+			jsonp("Q", jsonify(x.b)),
+			jsonp("expected", jsonify(x.e))
+		});
+	};
+	const auto jPAS = [](auto x) {
+		return jsonifyO({
+			x.desc.empty() ? "" : jsonp("description", jsonify(x.desc)),
+			jsonp("P", jsonify(x.a)),
+			jsonp("d", jsonify(x.b)),
+			x.except.empty() ? jsonp("expected", isNull(x.e) ? "null" : jsonify(x.e)) : "",
+			x.except.empty() ? "" : jsonp("exception", jsonify(x.except))
+		});
+	};
+	const auto jPFS = [](auto x) {
 		return jsonifyO({
 			x.desc.empty() ? "" : jsonp("description", jsonify(x.desc)),
 			jsonp("d", jsonify(x.a)),
-			jsonp("exception", jsonify(x.except))
+			x.except.empty() ? jsonp("expected", isNull(x.e) ? "null" : jsonify(x.e)) : "",
+			x.except.empty() ? "" : jsonp("exception", jsonify(x.except)),
 		});
 	};
-	const auto jPE = [](auto x) {
-		std::vector<std::string> kvs = {
-			jsonp("point", jsonify(x.a)),
-			jsonp("expected", jsonify(x.e))
-		};
-		if (!x.desc.empty()) kvs.push_back(jsonp("description", jsonify(x.desc)));
-		return jsonifyO(kvs);
-	};
-	const auto cnull = std::get<5>(compressed);
-	const auto unull = std::get<5>(uncompressed);
 
 	o << jsonifyO({
 		jsonp("valid", jsonifyO({
 			jsonp("isPoint", jsonifyA({
-				jsonify_csv(std::get<0>(compressed), jPE),
-				jsonify_csv(std::get<0>(uncompressed), jPE)
+				jsonify_csv(std::get<0>(compressed), jIP),
+				jsonify_csv(std::get<0>(uncompressed), jIP)
 			})),
 			jsonp("pointAdd", jsonifyA({
-				jsonify_csv(std::get<1>(compressed), jPDE(cnull)),
-				jsonify_csv(std::get<1>(uncompressed), jPDE(unull))
+				jsonify_csv(std::get<1>(compressed), jPA),
+				jsonify_csv(std::get<1>(uncompressed), jPA)
 			})),
 			jsonp("pointAddScalar", jsonifyA({
-				jsonify_csv(std::get<2>(compressed), jPDE(cnull)),
-				jsonify_csv(std::get<2>(uncompressed), jPDE(unull))
+				jsonify_csv(std::get<2>(compressed), jPAS),
+				jsonify_csv(std::get<2>(uncompressed), jPAS)
 			})),
 			jsonp("pointFromScalar", jsonifyA({
-				jsonify_csv(std::get<3>(compressed), jdE(cnull)),
-				jsonify_csv(std::get<3>(uncompressed), jdE(unull))
+				jsonify_csv(std::get<4>(compressed), jPFS),
+				jsonify_csv(std::get<4>(uncompressed), jPFS)
 			})),
 			jsonp("pointCompress", jsonifyA(pc, [](auto x) {
 				return jsonifyO({
@@ -259,9 +251,13 @@ void dumpJSON (
 			}))
 		})),
 		jsonp("invalid", jsonifyO({
+			jsonp("pointAddScalar", jsonifyA({
+				jsonify_csv(std::get<3>(compressed), jPAS),
+				jsonify_csv(std::get<3>(uncompressed), jPAS)
+			})),
 			jsonp("pointFromScalar", jsonifyA({
-				jsonify_csv(std::get<4>(compressed), jPFSF),
-				jsonify_csv(std::get<4>(uncompressed), jPFSF)
+				jsonify_csv(std::get<5>(compressed), jPFS),
+				jsonify_csv(std::get<5>(uncompressed), jPFS)
 			}))
 		}))
 	});
