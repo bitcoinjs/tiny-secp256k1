4ad4cbc3cfc8802543a2bb764d7857ce2c0e1111
Daniel Cousens <github@dcousens.com>
2018-06-25 16:08:29 +1000

    re-add toJSON function names

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 'use strict'
 
 try {
-  module.exports = require('bindings')('secp256k1')
+  let binding = require('bindings')('secp256k1')
+  delete binding.path
+
+  // NaN drops function names, add them for is* (via toJSON)
+  for (let key in binding) {
+    if (key.indexOf('is') !== 0) continue
+
+    binding[key].toJSON = function () { return key }
+  }
+
+  module.exports = binding
+
 } catch (err) {
   module.exports = require('./ecurve')
 }
