09b8dd4133021cf419e3b0304dc1cd616ad81494
junderw <junderwood@bitcoinbank.co.jp>
2021-10-13 22:20:00 +0900

    Fix: browser context randomization

--- src_ts/rand.browser.ts
+++ src_ts/rand.browser.ts
@@ -1,5 +1,10 @@
 export function generateInt32(): number {
   const array = new Uint8Array(4);
   window.crypto.getRandomValues(array);
-  return (array[0] << 3) + (array[1] << 2) + (array[2] << 1) + array[3];
+  return (
+    (array[0] << (3 * 8)) +
+    (array[1] << (2 * 8)) +
+    (array[2] << (1 * 8)) +
+    array[3]
+  );
 }
